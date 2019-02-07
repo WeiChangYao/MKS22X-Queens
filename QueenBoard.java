e2cd3d1333e0dfdb46f51efcc902846318a2bd05
weichangyao <wyao1@stuy.edu>
2019-02-07 10:00:42 -0500

    added stuff to constructor

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,7 +1,12 @@
 public class QueenBoard{
   private char[][]board;
   public QueenBoard(int size){
-    board = new int[size][size];
+    board = new char[size][size];
+    for (int i = 0; i < size; i++){
+      for (int j = 0; j < size; j++){
+        board[i][j] = '_';
+      }
+    }
   }
   private boolean addQueen(int r, int c){
     board[r][c] = 'Q';
