75ec49892a2d6829153baca1791955d3cdf6f066
weichangyao <wyao1@stuy.edu>
2019-02-08 11:19:05 -0500

    added toString

--- QueenBoard.java
+++ QueenBoard.java
@@ -1,15 +1,33 @@
 public class QueenBoard{
-  private char[][]board;
+  private int[][]board;
   public QueenBoard(int size){
-    board = new char[size][size];
+    board = new int[size][size];
     for (int i = 0; i < size; i++){
       for (int j = 0; j < size; j++){
-        board[i][j] = '_';
+        board[i][j] = 0;
       }
     }
   }
   private boolean addQueen(int r, int c){
-    board[r][c] = 'Q';
+    board[r][c] = -1;
     return true;
+  }
+  private boolean removeQueen(int r, int c){
+    board[r][c] = 0;
+    return true;
+  }
+  public String toString(){
+    String str = "";
+    for (int i = 0; i < size; i++){
+      for (int j = 0; j < size; j++){
+        if(board[i][j] == -1){
+          str += 'Q';
+        }
+        else{
+          str += '_';
+        }
+      }
+      str += '/n'
+    }
   }
 }
